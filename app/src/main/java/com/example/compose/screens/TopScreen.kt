fc2f29d0dfa529807b7d1ed1226a1012a06f0ad9
ELTEGANI <tiganimoh@gmail.com>
2023-08-14 21:45:56 +0400

    add input block

--- app/src/main/java/com/example/compose/screens/TopScreen.kt
+++ app/src/main/java/com/example/compose/screens/TopScreen.kt
@@ -1,10 +1,28 @@
 package com.example.compose.screens
 
+import android.util.Log
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import com.example.compose.models.Conversion
 
 @Composable
 fun TopScreen(listConverted: List<Conversion>) {
-   ConversionMenu(conversionList = listConverted, modifier = Modifier)
+   val selectedConversion : MutableState<Conversion?> = remember {
+      mutableStateOf(null)
+   }
+   val inputText : MutableState<String> = remember {
+      mutableStateOf("")
+   }
+   ConversionMenu(conversionList = listConverted, modifier = Modifier){
+       selectedConversion.value = it
+   }
+
+   selectedConversion.value?.let { 
+      InputBlock(conversion = it, inputText = inputText){input->
+
+      }
+   }
 }
